339daa69a8a859cb641cef23332e014219d0f719
Suraj Yadav <sy083609@gmail.com>
2025-07-15 19:24:41 +0530

    updated commit

--- Model_Inheritance/myproject/app/models.py
+++ Model_Inheritance/myproject/app/models.py
@@ -5,3 +5,7 @@ class MainModel(models.Model):
     name=models.CharField(max_length=50)
     email=models.EmailField()
     contact=models.IntegerField()
+
+class ProxyModel(MainModel):
+    class Meta:
+        proxy=True
